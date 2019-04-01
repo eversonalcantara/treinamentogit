c4c02ef6d78bdb225cfadf5316a828d74dfdbfd5
Everson Silva de Alcantara <everson.silva.alcantara@everis.com>
2019-04-01 17:50:39 -0300

    Adição de Linhas

--- src/Teste.java
+++ src/Teste.java
@@ -3,6 +3,10 @@ public class Teste {
 	
 	public static void main (String...args) {
 		System.out.println("EXITO");
+		System.out.println("EXITO");
+		System.out.println("EXITO");
+		System.out.println("EXITO");
+		System.out.println("EXITO");
 	}
 
 }
